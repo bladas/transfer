a0456fdd31097bc730a32fe7bb78d74328077837
bladas <dashkevich_v@ukr.net>
2019-11-29 00:42:04 +0200

    pochti gotov

--- apps/home/models.py
+++ apps/home/models.py
@@ -8,21 +8,35 @@ Start_Choices = (
 Type_car = (
     ('1', '1-4 пассажира,2-3 багажных места (легковой автомобиль)'),
     ('2', '5-6 пассажиров,4-5 багажных мест (минивэн)'),
-    ('2', '7-8 пассажиров,6-8 багажных мест (микроавтобус)')
+    ('3', '7-8 пассажиров,6-8 багажных мест (микроавтобус)')
 )
 
 Space = (
     ('1', 'КАМПЕЛЬО /EL CAMPELLO'),
     ('2', 'ВИЛЬЯХОЙОСА / VILLAJOYOSA'),
-    ('2', 'ФИНЕСТРАТ / FINESTRAT')
+    ('3', 'ФИНЕСТРАТ / FINESTRAT'),
+    ('4', 'БЕНИДОРМ / BENIDORM'),
+    ('5', 'АЛЬБИР / ALBIR'),
+    ('6', 'АЛТЕА / ALTEA'),
+    ('7', 'КАЛЬПЕ / CALPE'),
+    ('8', 'БЕНИССА / BENISSA'),
+    ('9', 'ТЕУЛАДА / TEULADA'),
+    ('10', 'МОРАЙРА / MORAIRA'),
+    ('11', 'ХАВЕА / JAVEA'),
+    ('12', 'ДЕНИЯ / DENIA'),
+    ('13', 'ОНДАРА / ONDARA'),
+    ('14', 'ОЛИВА / OLIVA'),
+    ('15', 'ГАНДИЯ / GANDIA')
 )
 
 
 class Calculate(models.Model):
     start = models.CharField(max_length=20, choices=Start_Choices)
-    type_car = models.CharField(max_length=50, choices=Type_car, default='1')
+    type_car = models.CharField(max_length=50, choices=Type_car)
     kuda = models.CharField(max_length=50, choices=Space)
     price = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
-        return self.start
+        return str(self.pk) + ' ' + self.price
+
+
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, render
 from django.views.generic import ListView, FormView
 from .models import Calculate
 from .forms import HomeForms
@@ -11,25 +11,22 @@ class HomeView(FormView):
     def get_queryset(self):
         return
 
-    def get_context_data(self, **kwargs):
 
-        context = super().get_context_data(**kwargs)
-        context['calculate'] = get_object_or_404(Calculate, pk=1)
-        return context
 
     def post(self, request, *args, **kwargs):
-        calculate_form = HomeForms(request.POST)
 
-        Calculate.objects.filter().update(price=None)
-        if (calculate_form.data.get('start') == '1'):
-            if (calculate_form.data.get('type_car') == '1'):
-                if (calculate_form.data.get('kuda') == '1'):
-                    Calculate.objects.filter().update(price='60')
-                    print('super')
+        calculate_form = HomeForms(request.POST)
 
         if calculate_form.is_valid():
 
-            return redirect('/')
+            print(calculate_form.data.get('start'))
+            print(calculate_form.data.get('kuda'))
+            print(calculate_form.data.get('type_car'))
+            context = super().get_context_data(**kwargs)
+            context['calculator'] = Calculate.objects.filter(start=calculate_form.data.get('start'), kuda=calculate_form.data.get('kuda'),
+                                     type_car=calculate_form.data.get('type_car'))
+
+            return render(request,'home.html',context)
         else:
             print(calculate_form.errors)
         print("0")
